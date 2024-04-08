228a524e02372a3365f6cafc8f0f412fd7ae8f57
ThalesCZ <thalescunha57@gmail.com>
2024-04-08 11:58:58 -0300

    criação-public=partials

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebase = require("firebase");
+
+const config = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
+
+firebase.initizalizeApp(config)
+
