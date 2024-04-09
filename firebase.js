c16aa1a4746339003a26bde3da4d3fb86428c6e7
Thales <thalescunha57@gmail.com>
2024-04-09 01:50:29 -0300

    autenticacao-funcionando

--- firebase.js
+++ firebase.js
@@ -9,5 +9,39 @@ const config = {
     messagingSenderId: process.env.MESSAGING_SENDER_ID
 };
 
-firebase.initizalizeApp(config)
+firebase.initializeApp(config);
 
+module.exports.SignUpWithEmailAndPassword = (email, password) => {
+    return firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+        return JSON.stringify(user)
+    })
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode == 'auth/weak-password') {
+            return {err: 'The password is too weak.'}
+        } else {
+          return {err: errorMessage }
+        }
+        return {err: error}
+    });
+}
+
+module.exports.SignInWithEmailAndPassword = (email, password) => {
+    return firebase.auth().signInWithEmailAndPassword(email, password)
+           .catch(function(error) {
+             // Handle Errors here.
+             var errorCode = error.code;
+             var errorMessage = error.message;
+             if (errorCode === 'auth/wrong-password') {
+               return {err: 'Wrong password.'}
+             } else {
+               return {err: errorMessage}
+             }
+             return {err: error}
+           });
+   }
+
+return module.exports
