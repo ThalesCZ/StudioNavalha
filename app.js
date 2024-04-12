aa3d6e00981538c558daac5747f8385469dfd2b3
ThalesCZ <thalescunha57@gmail.com>
2024-04-12 12:02:20 -0300

    autenticacao e index

--- app.js
+++ app.js
@@ -8,19 +8,38 @@ const ejs = require('ejs');
 
 let userLogged;
 const app = express()
-var publicDir = require('path').join(__dirname,'/public');
+var publicDir = require('path').join(__dirname, '/public');
+
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    next();
+});
+
+app.use((req, res, next) => {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            res.locals.userLogged = user;
+        } else {
+            res.locals.userLogged = null;
+        }
+        next(); 
+    });
+});
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(publicDir));
-app.use('/', express.static(__dirname + '/www')); 
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); 
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); 
+app.use('/', express.static(__dirname + '/www'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-app.use('/style', express.static(__dirname + '/style/')); 
+app.use('/style', express.static(__dirname + '/style/'));
 app.set('view engine', 'ejs');
 
 firebase.auth().onAuthStateChanged((user) => {
-    if(user){
+    if (user) {
         userLogged = user
     } else {
         userLogged = null
@@ -28,22 +47,26 @@ firebase.auth().onAuthStateChanged((user) => {
 })
 
 app.get('/', (req, res) => {
-    res.render('index', { signUpError: null, loginError: null });
-})
+    res.render('index', { userLogged: res.locals.userLogged });
+});
 
-app.get('/registro', function(req, res){
+app.get('/registro', function (req, res) {
     res.render('registro', { signUpError: null, loginError: null });
 });
 
-app.get('/login', function(req, res){
+app.get('/login', function (req, res) {
     res.render('login', { signUpError: null, loginError: null });
+});
+
+app.get('/agenda', function (req, res) {
+    res.render('agenda', { signUpError: null, loginError: null });
 });
 
 app.post('/createuser', (req, res) => {
     Auth.SignUpWithEmailAndPassword(req.body.email, req.body.password)
         .then((user) => {
             if (!user.err) {
-                let userData = JSON.parse(user); 
+                let userData = JSON.parse(user);
                 Auth.insertUserData(userData).then(() => {
                     res.redirect('/login');
                 }).catch(err => {
@@ -61,9 +84,6 @@ app.post('/createuser', (req, res) => {
         });
 });
 
-
-
-
 app.post('/login', (req, res) => {
     let getBody = req.body;
     Auth.SignInWithEmailAndPassword(getBody.email, getBody.password)
@@ -71,22 +91,28 @@ app.post('/login', (req, res) => {
             res.redirect('/');
         })
         .catch(loginError => {
-            let errorMessage = loginError.err === 'auth/wrong-password' ? "Senha incorreta." : "Erro ao fazer login.";
+            let errorMessage = "Usuário ou senha inválida!";
             res.render('login', { loginError: errorMessage, signUpError: null });
         });
 });
 
-
-
-app.get('/index', function(req, res){
-    if(userLogged){
-        Auth.GetData().then((data) => {
-            res.render('index', {data});
-        })
-    }else{
-        res.redirect('/login')
+app.get('/agendar', function (req, res) {
+    if (userLogged) {
+        res.render('agenda', { signUpError: null, loginError: null });
+    } else {
+        res.redirect('/login');
     }
 });
+
+app.get('/logout', (req, res) => {
+    firebase.auth().signOut().then(() => {
+        res.redirect('/login');
+    }).catch((error) => {
+        console.error('Erro durante o logout:', error);
+        res.redirect('/login');
+    });
+});
+
 
 
 
--- firebase.js
+++ firebase.js
@@ -1,71 +1,71 @@
-const firebase = require('firebase');
+  const firebase = require('firebase');
 
-var config = {
+  var config = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
     projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID
-};
+  };
 
-firebase.initializeApp(config);
+  firebase.initializeApp(config);
 
-module.exports.SignUpWithEmailAndPassword = (email, password) => {
-  return firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-      return JSON.stringify(userCredential.user); 
-  })
-  .catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      if (errorCode == 'auth/weak-password') {
-          return {err: 'auth/weak-password'};
-      } 
-      else if (errorCode = 'auth/email-already-in-use'){
-        return {err: 'auth/email-already-in-use'}
-      }
-      else {
-          return {err: errorMessage};
-      }
-  });
-}
-
-module.exports.SignInWithEmailAndPassword = (email, password) => {
-  return firebase.auth().signInWithEmailAndPassword(email, password)
-      .catch(function(error) {
-          var errorCode = error.code;
-          if (errorCode === 'auth/wrong-password') {
-              throw new Error('auth/wrong-password');
-          } else {
-              throw new Error(error.message); 
-          }
-      });
-}
-
-
-
-   module.exports.GetData = () => {
-     let data = []
-    return firebase.database().ref('users').once('value')
-    .then((snapshot) => {
-      
-      snapshot.forEach((childSnapshot)=>{
-        data.push({
-          id: childSnapshot.key,
-          ...childSnapshot.val() 
-        })
+  module.exports.SignUpWithEmailAndPassword = (email, password) => {
+    return firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        return JSON.stringify(userCredential.user);
       })
-      console.log(data)
-      return data;
-    })
+      .catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode == 'auth/weak-password') {
+          return { err: 'auth/weak-password' };
+        }
+        else if (errorCode = 'auth/email-already-in-use') {
+          return { err: 'auth/email-already-in-use' }
+        }
+        else {
+          return { err: errorMessage };
+        }
+      });
+  }
+
+  module.exports.SignInWithEmailAndPassword = (email, password) => {
+    return firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(function (error) {
+        var errorCode = error.code;
+        if (errorCode === 'auth/wrong-password') {
+          throw new Error('auth/wrong-password');
+        } else {
+          throw new Error(error.message);
+        }
+      });
+  }
+
+
+
+  module.exports.GetData = () => {
+    let data = []
+    return firebase.database().ref('users').once('value')
+      .then((snapshot) => {
+
+        snapshot.forEach((childSnapshot) => {
+          data.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+          })
+        })
+        console.log(data)
+        return data;
+      })
   }
 
   module.exports.insertUserData = (userData) => {
     return firebase.database().ref('users/' + userData.uid).set({
-        email: userData.email,
+      email: userData.email,
     });
-};
+  };
 
 
-return module.exports
+  return module.exports
