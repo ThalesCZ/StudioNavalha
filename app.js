c1856cc521f3b491adc8cefb830cce62c0ee4b55
ThalesCZ <thalescunha57@gmail.com>
2024-05-03 11:06:49 -0300

    zz

--- app.js
+++ app.js
@@ -170,16 +170,12 @@ app.post('/login', async (req, res) => {
 
 (async () => {
     try {
-      await sequelize.authenticate();
-      console.log('Conexão com o banco de dados estabelecida com sucesso!');
-      
-      await Cliente.sync();
-      
+      await sequelize.sync(); 
       console.log('Modelos sincronizados com o banco de dados!');
     } catch (error) {
-      console.error('Erro ao conectar ao banco de dados:', error);
+      console.error('Erro ao sincronizar modelos com o banco de dados:', error);
     }
-  })();
+})();
 
 app.use((req, res) => {
     res.redirect('/');
