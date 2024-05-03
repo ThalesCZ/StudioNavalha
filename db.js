6a237e43dda1ba751fab1e5878e7def1a5258a55
Thales <thalescunha57@gmail.com>
2024-05-03 20:28:05 -0300

    .

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true, 
-      rejectUnauthorized: false 
-    }
-  },
-  logging: false
-});
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'postgres',
+//   protocol: 'postgres',
+//   dialectOptions: {
+//     ssl: {
+//       require: true, 
+//       rejectUnauthorized: false 
+//     }
+//   },
+//   logging: false
+// });
 
-sequelize.authenticate()
-  .then(() => {
-    console.log('Conexão com o banco de dados estabelecida com sucesso.');
-  })
-  .catch(err => {
-    console.error('Não foi possível conectar ao banco de dados:', err);
-  });
+// sequelize.authenticate()
+//   .then(() => {
+//     console.log('Conexão com o banco de dados estabelecida com sucesso.');
+//   })
+//   .catch(err => {
+//     console.error('Não foi possível conectar ao banco de dados:', err);
+//   });
 
-module.exports = sequelize;
+// module.exports = sequelize;
--- models/cliente.js
+++ models/cliente.js
@@ -1,22 +1,22 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../db.js');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../db.js');
 
-class Cliente extends Model {}
+// class Cliente extends Model {}
 
-Cliente.init({
-  nome: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  }
-}, {
-  sequelize,
-  modelName: 'Cliente',
-  tableName: 'clientes'
-});
+// Cliente.init({
+//   nome: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//   email: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//     unique: true
+//   }
+// }, {
+//   sequelize,
+//   modelName: 'Cliente',
+//   tableName: 'clientes'
+// });
 
-module.exports = Cliente;
+// module.exports = Cliente;
