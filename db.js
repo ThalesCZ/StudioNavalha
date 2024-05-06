b75b144b0bfad912c390be3f81e11de680c9cd9c
ThalesCZ <thalescunha57@gmail.com>
2024-05-06 10:50:09 -0300

    .

--- db.js
+++ db.js
@@ -1,23 +1,20 @@
-// const { Sequelize } = require('sequelize');
+const { Pool } = require('pg');
+require('dotenv').config();
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: 'postgres',
-//   protocol: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//       require: true, 
-//       rejectUnauthorized: false 
-//     }
-//   },
-//   logging: false
-// });
+const pool = new Pool({
+  connectionString: "postgres://default:bS1ZtW6iMdqH@ep-frosty-glitter-a46qayt7-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
+});
 
-// sequelize.authenticate()
-//   .then(() => {
-//     console.log('Conexão com o banco de dados estabelecida com sucesso.');
-//   })
-//   .catch(err => {
-//     console.error('Não foi possível conectar ao banco de dados:', err);
-//   });
-
-// module.exports = sequelize;
+pool.query(`
+  CREATE TABLE IF NOT EXISTS cliente (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+  )
+`, (err, res) => {
+  if (err) {
+    console.error('Erro ao criar a tabela cliente:', err);
+  } else {
+    console.log('Tabela cliente criada com sucesso');
+  }
+});
