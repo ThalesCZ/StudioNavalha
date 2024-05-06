61251c770d6709d4cdeabbc3761823e3ad02c6cf
ThalesCZ <thalescunha57@gmail.com>
2024-05-06 11:43:02 -0300

    .

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ pool.query(`
   CREATE TABLE IF NOT EXISTS cliente (
     uid SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
   )
 `, (err, res) => {
   if (err) {
